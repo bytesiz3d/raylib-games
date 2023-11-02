3791c5fc896866ef273beaa0c60f95e44344e0cb
Abdelrahman Farid <abdelrahman.farid@gmail.com>
2023-11-02 22:43:22 +0200

    Improve snake

--- snake.c
+++ snake.c
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include <raymath.h>
+#include <memory.h>
 
 #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
@@ -23,16 +24,15 @@ CellEqual(Grid_Cell a, Grid_Cell b)
 }
 
 bool
-CellInside(Grid_Cell cell)
+CellOutOfBounds(Grid_Cell cell)
 {
-	return 0 <= cell.x && cell.x < GRID_W && 0 <= cell.y && cell.y < GRID_H;
+	return cell.x < 0 || cell.x >= GRID_W ||
+		   cell.y < 0 || cell.y >= GRID_H;
 }
 
 Grid_Cell snake[SNAKE_MAX] = {0};
 int snake_count;
-int snake_head;
-int snake_tail;
-#define SNAKE_INC(cell) (snake_tail + ((cell - snake_tail + 1) % snake_count))
+#define SNAKE_HEAD (snake_count-1)
 
 enum
 {
@@ -55,21 +55,20 @@ Grid_Cell
 RandomVacantCell()
 {
 	Grid_Cell cell = {0};
+	bool hit_snake_body = false;
 
 	for (int ITERATION_LIMIT = 0; ITERATION_LIMIT < GRID_W * GRID_H; ITERATION_LIMIT++)
 	{
 		cell = (Grid_Cell){GetRandomValue(0, GRID_W - 1), GetRandomValue(0, GRID_H - 1)};
 
-		bool hit_snake_body = false;
-		int i = snake_tail;
-		do
+		for (int i = 0; i < snake_count; ++i)
 		{
 			if (CellEqual(snake[i], apple_pos))
 			{
 				hit_snake_body = true;
 				break;
 			}
-		} while ((i = SNAKE_INC(i)) != snake_tail);
+		}
 
 		if (hit_snake_body == false)
 			return cell;
@@ -80,101 +79,94 @@ RandomVacantCell()
 void
 InitGame()
 {
-	apple_pos = RandomVacantCell();
-
 	snake[0] = (Grid_Cell){GRID_W / 2, GRID_H / 2};
 	snake[1] = (Grid_Cell){snake[0].x + 1, snake[0].y};
 	snake[2] = (Grid_Cell){snake[1].x + 1, snake[1].y};
-	snake_tail = 0;
-	snake_head = 2;
 	snake_count = 3;
 
 	snake_dir = SNAKE_RIGHT;
 	snake_state = SNAKE_ALIVE;
 	time_of_last_update = GetTime();
+
+	apple_pos = RandomVacantCell();
+}
+
+void
+Update(void)
+{
+	if (snake_state == SNAKE_DEAD)
+	{
+		if (IsKeyPressed(KEY_SPACE))
+			InitGame();
+
+		return;
+	}
+
+	for (int key; key = GetKeyPressed();)
+	{
+		if (key == KEY_RIGHT && snake_dir != SNAKE_LEFT)
+			snake_dir = SNAKE_RIGHT;
+		if (key == KEY_LEFT && snake_dir != SNAKE_RIGHT)
+			snake_dir = SNAKE_LEFT;
+		if (key == KEY_UP && snake_dir != SNAKE_DOWN)
+			snake_dir = SNAKE_UP;
+		if (key == KEY_DOWN && snake_dir != SNAKE_UP)
+			snake_dir = SNAKE_DOWN;
+	}
+
+	float time_now = GetTime();
+	if ((time_now - time_of_last_update) * SNAKE_SPEED >= 1)
+	{
+		time_of_last_update = time_now;
+
+		Grid_Cell head_new = snake[SNAKE_HEAD];
+		switch (snake_dir)
+		{
+		case SNAKE_LEFT: head_new.x -= 1; break;
+		case SNAKE_RIGHT: head_new.x += 1; break;
+		case SNAKE_UP: head_new.y -= 1; break;
+		case SNAKE_DOWN: head_new.y += 1; break;
+		}
+
+		if (CellOutOfBounds(head_new))
+		{
+			snake_state = SNAKE_DEAD;
+			return;
+		}
+		for (int i = 1; i < SNAKE_HEAD; ++i) // Ate self, skip last tail
+		{
+			if (CellEqual(head_new, snake[i]))
+			{
+				snake_state = SNAKE_DEAD;
+				return;
+			}
+		}
+
+		memcpy(snake, snake + 1, snake_count * sizeof(snake[0]));
+		snake[SNAKE_HEAD] = head_new;
+	}
+
+	if (CellEqual(snake[SNAKE_HEAD], apple_pos))
+	{
+		memcpy(snake + 1, snake, snake_count * sizeof(snake[0]));
+		snake_count++;
+		apple_pos = RandomVacantCell();
+	}
 }
 
 void
 UpdateDrawFrame(void)
 {
-	int key;
-	while (key = GetKeyPressed())
-	{
-		switch (key)
-		{
-		case KEY_RIGHT:
-			if (snake_dir != SNAKE_LEFT) snake_dir = SNAKE_RIGHT;
-			break;
-		case KEY_LEFT:
-			if (snake_dir != SNAKE_RIGHT) snake_dir = SNAKE_LEFT;
-			break;
-		case KEY_UP:
-			if (snake_dir != SNAKE_DOWN) snake_dir = SNAKE_UP;
-			break;
-		case KEY_DOWN:
-			if (snake_dir != SNAKE_UP) snake_dir = SNAKE_DOWN;
-			break;
-		}
-	}
-
-	if (IsKeyDown(KEY_SPACE) && snake_state == SNAKE_DEAD)
-		InitGame();
-
-	if (snake_state == SNAKE_ALIVE)
-	{
-		float time_now = GetTime();
-		if ((time_now - time_of_last_update) * SNAKE_SPEED >= 1)
-		{
-			time_of_last_update = time_now;
-
-			Grid_Cell head_new = snake[snake_head];
-			switch (snake_dir)
-			{
-			case SNAKE_LEFT: head_new.x -= 1; break;
-			case SNAKE_RIGHT: head_new.x += 1; break;
-			case SNAKE_UP: head_new.y -= 1; break;
-			case SNAKE_DOWN: head_new.y += 1; break;
-			}
-
-			snake_head = (snake_head + 1) % SNAKE_MAX;
-			snake_tail = (snake_tail + 1) % SNAKE_MAX;
-			snake[snake_head] = head_new;
-		}
-
-		if (CellEqual(snake[snake_head], apple_pos))
-		{
-			int snake_tail_new = (snake_tail - 1 + SNAKE_MAX) % SNAKE_MAX;
-			snake[snake_tail_new] = snake[snake_tail];
-			snake_tail = snake_tail_new;
-			snake_count += 1;
-			apple_pos = RandomVacantCell();
-		}
-		else if (!CellInside(snake[snake_head]))
-		{
-			snake_state = SNAKE_DEAD;
-			snake_head = (snake_head - 1 + SNAKE_MAX) % SNAKE_MAX; // Rewind one move
-			snake_tail = (snake_tail - 1 + SNAKE_MAX) % SNAKE_MAX;
-		}
-		else
-		{
-			for (int i = snake_tail; i != snake_head; i = SNAKE_INC(i))
-			{
-				if (CellEqual(snake[i], snake[snake_head]))
-				{
-					snake_state = SNAKE_DEAD;
-					break;
-				}
-			}
-		}
-	}
+	Update();
 
 	BeginDrawing();
 	{
 		ClearBackground(BLACK);
 
-		for (int i = snake_tail; i != snake_head; i = SNAKE_INC(i))
+		for (int i = 0; i < SNAKE_HEAD; ++i)
 			DrawRectangle(snake[i].x * GRID_SQUARE_SZ, snake[i].y * GRID_SQUARE_SZ, GRID_SQUARE_SZ, GRID_SQUARE_SZ, RAYWHITE);
-		DrawRectangle(snake[snake_head].x * GRID_SQUARE_SZ, snake[snake_head].y * GRID_SQUARE_SZ, GRID_SQUARE_SZ, GRID_SQUARE_SZ, LIGHTGRAY);
+
+		DrawRectangle(snake[SNAKE_HEAD].x * GRID_SQUARE_SZ, snake[SNAKE_HEAD].y * GRID_SQUARE_SZ, GRID_SQUARE_SZ, GRID_SQUARE_SZ, LIGHTGRAY);
 
 		DrawRectangle(apple_pos.x * GRID_SQUARE_SZ, apple_pos.y * GRID_SQUARE_SZ, GRID_SQUARE_SZ, GRID_SQUARE_SZ, RED);
 
